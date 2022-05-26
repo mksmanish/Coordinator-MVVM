d275e250125eedd4c9c2e6e552e1c653af087ee3
Manish Kumar <manish.kumar@tradesocio.com>
2022-05-26 14:11:16 +0530

    testing unit

--- CoordinatorTests/CoordinatorTests.swift
+++ CoordinatorTests/CoordinatorTests.swift
@@ -10,7 +10,7 @@ import XCTest
 
 class CoordinatorTests: XCTestCase {
     var eventDetails : EventListViewController!
-
+    
     override func setUp(){
         super.setUp()
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
@@ -23,25 +23,29 @@ class CoordinatorTests: XCTestCase {
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
+    
     func testHasATableView() {
         XCTAssertNotNil(eventDetails.tblEventList)
-      }
-      
-      func testTableViewHasDelegate() {
-          XCTAssertNotNil(eventDetails.tblEventList.delegate)
-      }
-      
-      func testTableViewHasDataSource() {
-          XCTAssertNotNil(eventDetails.tblEventList.dataSource)
-      }
-      
-      func testemployeeData() {
-          XCTAssertNotNil(eventDetails.viewModel.numberOfRows)
-      }
-
+    }
+    
+    func testTableViewHasDelegate() {
+        XCTAssertNotNil(eventDetails.tblEventList.delegate)
+    }
+    
+    func testTableViewHasDataSource() {
+        XCTAssertNotNil(eventDetails.tblEventList.dataSource)
+    }
+    
+    func testemployeeData() {
+        XCTAssertNotNil(eventDetails.viewModel.numberOfRows)
+    }
+    
+    func testmath() {
+        XCTAssertEqual(2+3, 5)
+    }
+    
 }
